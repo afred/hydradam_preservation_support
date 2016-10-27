214ba383dbb4dc438322e0e486c807e63d00d394
Andrew Myers <afredmyers@gmail.com>
2016-10-27 00:00:20 -0600

    Add assoc from PreservationEvent to FileSet
    Also adds a class method to return a convenience hash of all PREMIS event types.

--- app/models/preservation_event.rb
+++ app/models/preservation_event.rb
@@ -3,4 +3,16 @@ require 'rdf/vocab'
 class PreservationEvent < ActiveFedora::Base
   type ::RDF::Vocab::PREMIS.Event
   property :premis_event_type, predicate: ::RDF::Vocab::PREMIS.hasEventType
+  property :premis_event_related_object, predicate: ::RDF::Vocab::PREMIS.hasEventRelatedObject
+
+  def self.premis_event_types
+    @premis_event_types ||= begin
+      {}.tap do |premis_event_types|
+        [:cap, :com, :cre, :dea, :dec, :del, :der, :dig, :fix, :ing, :mes,
+          :mig, :nor, :rep, :val, :vir].each do |type|
+          premis_event_types[type] = ::RDF::Vocab::PremisEventType.send(type)
+        end
+      end
+    end
+  end
 end
